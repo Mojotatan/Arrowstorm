a1111a68db668b9f6869e7ed5c91afab68b4eafc
Mojotatan <tatan42@gmail.com>
2017-05-04 14:01:04 -0500

    player creation is a seperate function, fat kid sprite added

--- public/js/create/create.js
+++ public/js/create/create.js
@@ -1,114 +1,101 @@
+import createPlayer from './player'
+
 export default function createFunc(d) {
 
-    //add background 
-    d.game.add.image(0, 0, 'background')
-    //  We're going to be using physics, so enable the Arcade Physics system
-    d.game.physics.startSystem(Phaser.Physics.ARCADE);
-    //set platforms
-    d.platforms = d.game.add.group()
+  //add background 
+  d.game.add.image(0, 0, 'background')
+  //  We're going to be using physics, so enable the Arcade Physics system
+  d.game.physics.startSystem(Phaser.Physics.ARCADE);
+  //set platforms
+  d.platforms = d.game.add.group()
 
-    //enable physics on platforms
-    d.platforms.enableBody = true
-    d.platforms.physicsBodyType = Phaser.Physics.ARCADE
+  //enable physics on platforms
+  d.platforms.enableBody = true
+  d.platforms.physicsBodyType = Phaser.Physics.ARCADE
 
-    //creating the ground
-    let ground = d.platforms.create(0, d.game.world.height - 64, 'ground')
-    let ground2 = d.platforms.create(250, d.game.world.height - 64, 'ground')
+  //creating the ground
+  let ground = d.platforms.create(0, d.game.world.height - 64, 'ground')
+  let ground2 = d.platforms.create(250, d.game.world.height - 64, 'ground')
 
-    //scaling the ground
-    ground.scale.setTo(2, 2)
-    ground2.scale.setTo(2, 2)
+  //scaling the ground
+  ground.scale.setTo(2, 2)
+  ground2.scale.setTo(2, 2)
 
-    ground.body.immovable = true
-    ground2.body.immovable = true
+  ground.body.immovable = true
+  ground2.body.immovable = true
 
-    // Creating left brick wall
-    d.leftWall = d.game.add.group()
-    d.leftWall.enableBody = true
+  // Creating left brick wall
+  d.leftWall = d.game.add.group()
+  d.leftWall.enableBody = true
 
-    for (let i = 0; i < 9; i++) {
-        var leftBlockStack = d.leftWall.create(99, i * 60.5, 'brick')
+  for (let i = 0; i < 5; i++) {
+      var leftBlockStack = d.leftWall.create(64, i * 32 * 4, 'brick')
 
-        leftBlockStack.scale.setTo(3, 3)
+      leftBlockStack.scale.setTo(4, 4)
 
-        leftBlockStack.body.immovable = true
+      leftBlockStack.body.immovable = true
 
-        //leftBlockStack.gravity.y = 6
-    }
-
-    //creating right wall
-    d.rightWall = d.game.add.group()
-    d.rightWall.enableBody = true
-
-    for (let j = 0; j < 9; j++) {
-        var rightBlockStack = d.leftWall.create(837, j * 60.5, 'brick')
-
-        rightBlockStack.scale.setTo(3, 3)
-        rightBlockStack.body.immovable = true
-    }
-
-
-    //creating ledge 
-    let ledge = d.platforms.create(450, 425, 'grassBlockLedge');
-    ledge.body.immovable = true
-
-    //creating walls 
-    let wall = d.platforms.create(0, 580, 'ground');
-    wall.rotation = 23.565
-    wall.scale.setTo(3, 3)
-    wall.body.immovable = true
-
-    let wall2 = d.platforms.create(930, 600, 'ground')
-    wall2.rotation = 23.565
-    wall2.scale.setTo(3, 3)
-    wall2.body.immovable = true
-
-    let ledgeLeft = d.platforms.create(180, 300, 'grassBlockLedge')
-    ledgeLeft.body.immovable = true
-
-    let upperLedge = d.platforms.create(450, 200, 'grassBlock')
-    upperLedge.body.immovable = true
-
-
-    let ledgeRight = d.platforms.create(d.game.world.width - 192 - 84, 100, 'grassBlock')
-    ledgeRight.body.immovable = true
-
-    d.roboraj = d.game.add.sprite(128, 0, 'roboraj')
-    d.roboraj.scale.set(1.5, 1.5)
-    d.roboraj.pivot.set(15, 0)
-    d.game.physics.arcade.enable(d.roboraj)
-    d.roboraj.body.gravity.y = 400
-    // d.roboraj.body.bounce.y = 0.3
-    d.roboraj.body.collideWorldBounds = true
-
-    d.roboraj.animations.add('walk', [0, 1], 10, true)
-
-    d.bow = d.game.add.sprite(0, 0, 'bow')
-    d.bow.anchor.set(1, .5)
-    // d.bow.position.set
-    d.roboraj.addChild(d.bow)
-    // d.bow.pivot.set(16, 16)
-    // d.bow.rotation = .785 + .785
-    // character sprites should be 20 by 32
-
-
-    // Create arrow group
-    d.arrows = d.game.add.group()
-    d.arrows.enableBody = true
-    d.arrows.physicsBodyType = Phaser.Physics.ARCADE
-
-    d.arrows.createMultiple(1, 'arrow')
-    d.arrows.callAll('events.onOutOfBounds.add', 'events.onOutOfBounds', resetArrow);
-    d.arrows.callAll('anchor.setTo', 'anchor', 0.5, 1.0)
-    d.arrows.setAll('checkWorldBounds', true)
-
-    d.spaceBar = d.game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR)
-    d.game.input.keyboard.addKeyCapture(Phaser.KeyCode.SPACEBAR)
-
-    d.game.stage.backgroundColor = "#999999"
-
-
+      //leftBlockStack.gravity.y = 6
   }
+
+  //creating right wall
+  d.rightWall = d.game.add.group()
+  d.rightWall.enableBody = true
+
+  for (let j = 0; j < 5; j++) {
+      var rightBlockStack = d.leftWall.create(832, j * 32 * 4, 'brick')
+
+      rightBlockStack.scale.setTo(4, 4)
+      rightBlockStack.body.immovable = true
+  }
+
+
+  //creating ledge 
+  let ledge = d.platforms.create(450, 425, 'grassBlockLedge');
+  ledge.body.immovable = true
+
+  //creating walls 
+  let wall = d.platforms.create(0, 580, 'ground');
+  wall.rotation = 23.565
+  wall.scale.setTo(3, 3)
+  wall.body.immovable = true
+
+  let wall2 = d.platforms.create(930, 600, 'ground')
+  wall2.rotation = 23.565
+  wall2.scale.setTo(3, 3)
+  wall2.body.immovable = true
+
+  let ledgeLeft = d.platforms.create(180, 300, 'grassBlockLedge')
+  ledgeLeft.body.immovable = true
+
+  let upperLedge = d.platforms.create(450, 200, 'grassBlock')
+  upperLedge.body.immovable = true
+
+
+  let ledgeRight = d.platforms.create(d.game.world.width - 192 - 84, 100, 'grassBlock')
+  ledgeRight.body.immovable = true
+
+
+  createPlayer(d, 'fatKid', 'player1', {x: 128, y: 0})
+
+
+  // Create arrow group
+  d.arrows = d.game.add.group()
+  d.arrows.enableBody = true
+  d.arrows.physicsBodyType = Phaser.Physics.ARCADE
+
+  d.arrows.createMultiple(1, 'arrow')
+  d.arrows.callAll('events.onOutOfBounds.add', 'events.onOutOfBounds', resetArrow);
+  d.arrows.callAll('anchor.setTo', 'anchor', 0.5, 1.0)
+  d.arrows.setAll('checkWorldBounds', true)
+
+  d.spaceBar = d.game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR)
+  d.game.input.keyboard.addKeyCapture(Phaser.KeyCode.SPACEBAR)
+
+  d.game.stage.backgroundColor = "#999999"
+
+
+}
 
 export function resetArrow(arrow) {
     arrow.kill()
--- public/js/update/update.js
+++ public/js/update/update.js
@@ -3,8 +3,8 @@ import {findA} from '../util'
 export default function updateFunc(d) {
 
     //Define collisions
-    let hitPlatform = d.game.physics.arcade.collide(d.platforms, d.roboraj)
-    let hitBricks = d.game.physics.arcade.collide(d.leftWall, d.roboraj)
+    let hitPlatform = d.game.physics.arcade.collide(d.platforms, d.player1)
+    let hitBricks = d.game.physics.arcade.collide(d.leftWall, d.player1)
 
 
     // initializing cursor
@@ -15,44 +15,45 @@ export default function updateFunc(d) {
     d.aimDown = d.game.input.keyboard.addKey(Phaser.Keyboard.S)
 
     //stand still
-    d.roboraj.body.velocity.x = 0
+    d.player1.body.velocity.x = 0
     d.bow.rotation = 0
-    d.bow.position.set(8, 16)
+    d.bow.position.set(2, 16)
 
     if (cursors.left.isDown)
     {
-      d.roboraj.body.velocity.x = -350
+      d.player1.body.velocity.x = -300
 
-      if (d.roboraj.scale.x < 0) d.roboraj.scale.x *= -1
+      if (d.player1.scale.x < 0) d.player1.scale.x *= -1
 
-      d.roboraj.animations.play('walk')
+      d.player1.animations.play('walk')
 
     }
     else if (cursors.right.isDown)
     {
         //  Move to the right
-        d.roboraj.body.velocity.x = 350
+        d.player1.body.velocity.x = 300
 
-        if (d.roboraj.scale.x > 0) d.roboraj.scale.x *= -1
+        if (d.player1.scale.x > 0) d.player1.scale.x *= -1
 
-        d.roboraj.animations.play('walk')
+        d.player1.animations.play('walk')
     }
     else {
-      d.roboraj.animations.stop()
+      d.player1.animations.stop()
+      d.player1.frame = 2
     }
 
-    if((cursors.up.isDown) && d.roboraj.body.touching.down && hitPlatform) {
-      d.roboraj.body.velocity.y = -350
+    if((cursors.up.isDown) && d.player1.body.touching.down && hitPlatform) {
+      d.player1.body.velocity.y = -500
     }
 
     // you can turn your player by either moving in a direction or by aiming in a direction
     // the direction you aim in takes precedent over the direction you move in
     // which allows for strafing
     if (d.aimLeft.isDown) {
-      if (d.roboraj.scale.x < 0) d.roboraj.scale.x *= -1
+      if (d.player1.scale.x < 0) d.player1.scale.x *= -1
     }
     else if (d.aimRight.isDown) {
-      if (d.roboraj.scale.x > 0) d.roboraj.scale.x *= -1
+      if (d.player1.scale.x > 0) d.player1.scale.x *= -1
     }
 
     // for the diagonal directions
@@ -61,29 +62,29 @@ export default function updateFunc(d) {
     if (d.aimDown.isDown) {
       if (d.aimDown.isDown && d.aimLeft.isDown) {
         d.bow.rotation = -.785
-        d.bow.position.set(16 - distanceFromCenter, 16 + distanceFromCenter)
+        d.bow.position.set(10 - distanceFromCenter, 16 + distanceFromCenter)
       }
       else if (d.aimDown.isDown && d.aimRight.isDown) {
         d.bow.rotation = -.785
-        d.bow.position.set(16 - distanceFromCenter, 16 + distanceFromCenter)
+        d.bow.position.set(10 - distanceFromCenter, 16 + distanceFromCenter)
       }
       else {
         d.bow.rotation = -1.57
-        d.bow.position.set(16, 24)
+        d.bow.position.set(10, 24)
       }
     }
     if (d.aimUp.isDown) {
       if (d.aimUp.isDown && d.aimLeft.isDown) {
         d.bow.rotation = .785
-        d.bow.position.set(16 - distanceFromCenter, 16 - distanceFromCenter)
+        d.bow.position.set(10 - distanceFromCenter, 16 - distanceFromCenter)
       }
       else if (d.aimUp.isDown && d.aimRight.isDown) {
         d.bow.rotation = .785
-        d.bow.position.set(16 - distanceFromCenter, 16 - distanceFromCenter)
+        d.bow.position.set(10 - distanceFromCenter, 16 - distanceFromCenter)
       }
       else {
         d.bow.rotation = 1.57
-        d.bow.position.set(16, 8)
+        d.bow.position.set(10, 8)
       }
     }
 
@@ -101,7 +102,7 @@ function fireArrow(d) {
         arrow.animations.play('fire')
         arrow.body.gravity.y = 400
         //arrow.body.collideWorldBounds = true
-        arrow.reset(d.roboraj.x, d.roboraj.y)
+        arrow.reset(d.player1.x, d.player1.y)
 
 
         if (d.aimUp.isDown) {
